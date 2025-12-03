b322a20275570afe2f26ce236fb9a3a7c04ef3eb
gabrielmirandasilv <gabrielmiranda61637@gmail.com>
2025-12-02 21:15:51 -0300

    Spring Initialzr

---
+++ simuladorbrasileirao/src/main/java/com/gabrielmirandasilv/simuladorbrasileirao/SimuladorbrasileiraoApplication.java
@@ -0,0 +1,13 @@
+package com.gabrielmirandasilv.simuladorbrasileirao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimuladorbrasileiraoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimuladorbrasileiraoApplication.class, args);
+	}
+
+}
---
+++ simuladorbrasileirao/src/test/java/com/gabrielmirandasilv/simuladorbrasileirao/SimuladorbrasileiraoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabrielmirandasilv.simuladorbrasileirao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimuladorbrasileiraoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
